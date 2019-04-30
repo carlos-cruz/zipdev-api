a863a81a2540d8661f88eaea4ef2a05c0f9e010e
Carlos Cruz <jccs24@gmail.com>
2019-04-29 19:29:34 -0700

    DB Dump for zipdev-contacts-api

---
+++ db/zipdev_contacts.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.6.41, for Linux (x86_64)
+--
+-- Host: localhost    Database: zipdev_contacts
+-- ------------------------------------------------------
+-- Server version	5.6.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `zipdev_contacts`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `zipdev_contacts` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `zipdev_contacts`;
+
+--
+-- Table structure for table `emails`
+--
+
+DROP TABLE IF EXISTS `emails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emails` (
+  `phonebook_id` int(11) unsigned NOT NULL,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  KEY `emails_phonebook_id_phonebook_foreign` (`phonebook_id`),
+  CONSTRAINT `emails_phonebook_id_phonebook_foreign` FOREIGN KEY (`phonebook_id`) REFERENCES `phonebook` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emails`
+--
+
+LOCK TABLES `emails` WRITE;
+/*!40000 ALTER TABLE `emails` DISABLE KEYS */;
+INSERT INTO `emails` VALUES (1,'carlos.cruz@hotmail.com'),(1,'carloscruz@gmail.com');
+/*!40000 ALTER TABLE `emails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phonebook`
+--
+
+DROP TABLE IF EXISTS `phonebook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phonebook` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT '',
+  `sur_name` varchar(100) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phonebook`
+--
+
+LOCK TABLES `phonebook` WRITE;
+/*!40000 ALTER TABLE `phonebook` DISABLE KEYS */;
+INSERT INTO `phonebook` VALUES (1,'Carlos','Cruz');
+/*!40000 ALTER TABLE `phonebook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phones`
+--
+
+DROP TABLE IF EXISTS `phones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phones` (
+  `phonebook_id` int(11) unsigned NOT NULL,
+  `phone` varchar(20) NOT NULL DEFAULT '',
+  KEY `phones_phonebook_id_phonebook_foreign` (`phonebook_id`),
+  CONSTRAINT `phones_phonebook_id_phonebook_foreign` FOREIGN KEY (`phonebook_id`) REFERENCES `phonebook` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phones`
+--
+
+LOCK TABLES `phones` WRITE;
+/*!40000 ALTER TABLE `phones` DISABLE KEYS */;
+INSERT INTO `phones` VALUES (1,'6641234567'),(1,'6649876543');
+/*!40000 ALTER TABLE `phones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-30  9:27:30
